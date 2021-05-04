888a89f75ea254576af093c9ded8cdf7c89e9137
Russell Horth <russell.horth@gmail.com>
2021-05-04 15:40:41 +0100

    step 4 , add an initial balance

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,4 +1,10 @@
 
+require './lib/oystercard'
 describe Oystercard do 
+
+  it 'tests a new Oystercard balance is 0' do 
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq 0
+  end
 
 end
